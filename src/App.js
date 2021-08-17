1c767b8efc615fd9f2515a3584122dad1974728c
smilegupta <guptamiley3012@gmail.com>
2021-08-17 15:21:39 +0530

    Page Routing and Code Formatted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
-  return <div>I am bill book app</div>;
+  return (
+    <Fragment>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            I am Customer's page
+          </Route>
+          <Route path="/inventory">I am Inventory page</Route>
+          <Route path="/invoice">I am Invoice page</Route>
+        </Switch>
+      </Router>
+    </Fragment>
+  );
 }
 
 export default App;
