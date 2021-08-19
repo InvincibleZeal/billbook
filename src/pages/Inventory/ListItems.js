8c9a8786bf3ba0fd18e9ae9243e678617d55daf8
smilegupta <guptamiley3012@gmail.com>
2021-08-19 11:21:45 +0530

    fix(listitems): fixed the bug in list items page

--- src/pages/Inventory/ListItems.js
+++ src/pages/Inventory/ListItems.js
@@ -16,57 +16,61 @@ const ListItems = () => {
                         </button>
                     </Link>
                 </div>
-                <table className="table px-5">
-                    <colgroup>
-                        <col span="1" style={{ width: "20%" }} />
-                        <col span="1" style={{ width: "50%" }} />
-                        <col span="1" style={{ width: "15%" }} />
-                        <col span="1" style={{ width: "15%" }} />
-                    </colgroup>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Description</th>
-                            <th>Price</th>
-                            <th>Added On</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Anissa Beier</td>
-                            <td>
-                                Lorem ipsum dolor sit, amet consectetur
-                                adipisicing elit. Tempore, sapiente praesentium
-                                deleniti placeat voluptas architecto!
-                            </td>
-                            <td>₹350</td>
-                            <td>06 Aug 2020</td>
-                        </tr>
-                        <tr>
-                            <td>Mellie Buckride</td>
-                            <td>
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Accusamus, aut!
-                            </td>
-                            <td>₹800</td>
-                            <td>06 Aug 2020</td>
-                        </tr>
-                        <tr>
-                            <td>Ashlee Glover</td>
-                            <td>
-                                Lorem ipsum dolor sit amet consectetur,
-                                adipisicing elit. Iste vel nemo voluptatibus ut
-                                eius rerum doloribus itaque. Sapiente
-                                consequatur iusto autem? Placeat, enim molestias
-                                magni aspernatur aperiam itaque repellat
-                                blanditiis ut qui ipsam temporibus cumque
-                                numquam distinctio illo doloribus quisquam?
-                            </td>
-                            <td>₹80</td>
-                            <td>06 Aug 2020</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div className="scrollable">
+                    <table className="table px-5">
+                        <colgroup>
+                            <col span="1" style={{ width: "20%" }} />
+                            <col span="1" style={{ width: "50%" }} />
+                            <col span="1" style={{ width: "15%" }} />
+                            <col span="1" style={{ width: "15%" }} />
+                        </colgroup>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Price</th>
+                                <th>Added On</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Anissa Beier</td>
+                                <td>
+                                    Lorem ipsum dolor sit, amet consectetur
+                                    adipisicing elit. Tempore, sapiente
+                                    praesentium deleniti placeat voluptas
+                                    architecto!
+                                </td>
+                                <td>₹350</td>
+                                <td>06 Aug 2020</td>
+                            </tr>
+                            <tr>
+                                <td>Mellie Buckride</td>
+                                <td>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Accusamus, aut!
+                                </td>
+                                <td>₹800</td>
+                                <td>06 Aug 2020</td>
+                            </tr>
+                            <tr>
+                                <td>Ashlee Glover</td>
+                                <td>
+                                    Lorem ipsum dolor sit amet consectetur,
+                                    adipisicing elit. Iste vel nemo voluptatibus
+                                    ut eius rerum doloribus itaque. Sapiente
+                                    consequatur iusto autem? Placeat, enim
+                                    molestias magni aspernatur aperiam itaque
+                                    repellat blanditiis ut qui ipsam temporibus
+                                    cumque numquam distinctio illo doloribus
+                                    quisquam?
+                                </td>
+                                <td>₹80</td>
+                                <td>06 Aug 2020</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </Fragment>
     );
