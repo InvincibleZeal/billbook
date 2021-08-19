14cdcd23727a2e6ca8c569f7de7e6a68e090636d
smilegupta <guptamiley3012@gmail.com>
2021-08-19 07:53:21 +0530

    feat(app.js): added Routing for AddItem Page and Create Invoice Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ListCustomers, AddCustomer } from "./pages/Customers";
-import { ListItems } from "./pages/Inventory";
-import { ListInvoices } from "./pages/Invoice";
+import { ListItems, AddItem } from "./pages/Inventory";
+import { ListInvoices, CreateInvoice } from "./pages/Invoice";
 
 function App() {
     return (
         <Router>
             <Switch>
+                {/* Customer Module */}
                 <Route path="/" exact>
                     <ListCustomers />
                 </Route>
                 <Route path="/add-customer">
                     <AddCustomer />
                 </Route>
+                {/* Inventory Module */}
                 <Route path="/inventory">
                     <ListItems />
                 </Route>
+                <Route path="/add-item">
+                    <AddItem />
+                </Route>
+                {/* Invoice Module */}
                 <Route path="/invoice">
                     <ListInvoices />
+                </Route>
+                <Route path="/create-invoice">
+                    <CreateInvoice />
                 </Route>
             </Switch>
         </Router>
