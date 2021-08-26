4c79405648c5089d9783ff7b6ed47097a708d44d
smilegupta <guptamiley3012@gmail.com>
2021-08-26 10:01:51 +0530

    refactor(useform): added Callback function in  useForm

--- src/customHooks/useForm.js
+++ src/customHooks/useForm.js
@@ -1,21 +1,28 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 export function useForm(initialState) {
     const [fields, setValues] = useState(initialState);
 
-    return [
-        fields,
-        function (event) {
+    // Function to handle form values
+    const handleFormState = useCallback(
+        (event) => {
             setValues((prev) => ({
                 ...prev,
                 [event.target.name]: event.target.value,
             }));
         },
-        function (key, value) {
+        [fields]
+    );
+
+    // Function to handle object, array in fields
+    const handleState = useCallback(
+        (key, value) => {
             setValues((prev) => ({
                 ...prev,
                 [key]: value,
             }));
         },
-    ];
+        [fields]
+    );
+    return [fields, handleFormState, handleState];
 }
