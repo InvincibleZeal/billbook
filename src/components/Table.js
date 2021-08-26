2f70a2bf5e504a23ce710f33460b13973612f4ee
Vipin <vipin.gautam@razorpay.com>
2021-08-26 11:43:26 +0530

    fix(table.js): remove repitive code

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,30 +2,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 function Table({ data, formatter }) {
-    console.log();
-    const Theads = formatter.map((theadValue) => (
-        <th key3> {theadValue.label}</th>
-    ));
+    const Theads = formatter.map((theadValue) => <th> {theadValue.label}</th>);
 
     const TableBody = data.map((rowData) => {
-        let cellData = "";
         const trData = formatter.map((columnData) => {
-            if (columnData.id === "undefined") {
-                if (
-                    columnData.formatter &&
-                    typeof columnData.formatter === "function" // this is optional if we are sure that if formatter exist then it will be a function
-                ) {
-                    cellData = columnData.formatter(rowData, data);
+            let cellData = "";
+            const columnDataId = columnData.id;
+            const columnDataFormatterValidator =
+                columnData.formatter &&
+                typeof columnData.formatter === "function"; // this is optional if we are sure that if formatter exist then it will be a function
+
+            if (!columnDataId) {
+                if (columnDataFormatterValidator) {
+                    cellData = columnData.formatter(data, rowData);
                     return <td>{cellData}</td>;
                 } else {
                     return <td></td>;
                 }
             }
-            if (columnData.id) cellData = rowData[columnData.id];
-            if (
-                columnData.formatter &&
-                typeof columnData.formatter === "function" // condition can be removed
-            ) {
+            if (columnDataId) cellData = rowData[columnDataId];
+            if (columnDataFormatterValidator) {
                 cellData = columnData.formatter(columnData, rowData);
                 return <td>{cellData}</td>;
             }
