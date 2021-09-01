68e4d36e4148fb8a7161620351a02843adca5c9f
Arshpreet <arshpreet.wadehra@razorpay.com>
2021-09-01 17:55:35 +0530

    test: coverage calculation support added

--- src/components/__tests__/Card.test.js
+++ src/components/__tests__/Card.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import Card from "components/Card";
+import Card from "../Card";
 
 describe("Card", () => {
     it("should not be an empty DOM element", () => {
