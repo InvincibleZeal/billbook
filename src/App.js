7056b0fdec9685e1246d479efd12cdb9ab8be648
Vipin Gautam <35873526+gautism1@users.noreply.github.com>
2021-09-06 16:09:32 +0530

    feat(app.js): lazy loading of components (#128)
    * feat(app.js): lazy loading of componnets
    
    * feat(app.js): route level lazy loading of components
    
    Co-authored-by: Vipin <vipin.gautam@razorpay.com>

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect, useCallback, Suspense, lazy } from "react";
 import {
     BrowserRouter as Router,
     Route,
     Switch,
     Redirect,
 } from "react-router-dom";
-import { ListCustomers, AddCustomer } from "pages/Customers";
-import { ListItems, AddItem } from "pages/Inventory";
-import { ListInvoices, CreateInvoice } from "pages/Invoice";
+
 import { NotificationProvider } from "notification";
 import withWrapper from "common/withWrapper";
 import Navbar from "common/Navbar";
 import ErrorBoundary from "common/ErrorBoundary";
 import { useDispatch } from "react-redux";
 import { fetchCustomersList, fetchItemsList } from "redux/actions/index";
+const AddCustomer = lazy(() => import("./pages/Customers/AddCustomer.js"));
+const ListCustomers = lazy(() => import("./pages/Customers/ListCustomers"));
+const ListItems = lazy(() => import("./pages/Inventory/ListItems"));
+const AddItem = lazy(() => import("./pages/Inventory/AddItem"));
+const ListInvoices = lazy(() => import("./pages/Invoice/ListInvoices"));
+const CreateInvoice = lazy(() => import("./pages/Invoice/CreateInvoice"));
 
 function App() {
     // State Variables
@@ -35,33 +39,35 @@ function App() {
             <NotificationProvider>
                 <Router>
                     <Navbar />
-                    <Switch>
-                        {/* Customer Module */}
-                        <Route path="/" exact>
-                            <Redirect to="/customers" />
-                        </Route>
-                        <Route path="/customers" exact>
-                            <ListCustomers />
-                        </Route>
-                        <Route path="/customers/add">
-                            <AddCustomer />
-                        </Route>
-                        {/* Inventory Module */}
-                        <Route path="/inventory" exact>
-                            <ListItems />
-                        </Route>
-                        <Route path="/inventory/add">
-                            <AddItem />
-                        </Route>
-                        {/* Invoice Module */}
-                        <Route path="/invoice" exact>
-                            <ListInvoices />
-                        </Route>
-                        <Route path="/invoice/add">
-                            <CreateInvoice />
-                        </Route>
-                        <Route component={ListCustomers} />
-                    </Switch>
+                    <Suspense fallback={<h1>Loading components</h1>}>
+                        <Switch>
+                            {/* Customer Module */}
+                            <Route path="/" exact>
+                                <Redirect to="/customers" />
+                            </Route>
+                            <Route path="/customers" exact>
+                                <ListCustomers />
+                            </Route>
+                            <Route path="/customers/add">
+                                <AddCustomer />
+                            </Route>
+                            {/* Inventory Module */}
+                            <Route path="/inventory" exact>
+                                <ListItems />
+                            </Route>
+                            <Route path="/inventory/add">
+                                <AddItem />
+                            </Route>
+                            {/* Invoice Module */}
+                            <Route path="/invoice" exact>
+                                <ListInvoices />
+                            </Route>
+                            <Route path="/invoice/add">
+                                <CreateInvoice />
+                            </Route>
+                            <Route component={ListCustomers} />
+                        </Switch>
+                    </Suspense>
                 </Router>
             </NotificationProvider>
         </ErrorBoundary>
